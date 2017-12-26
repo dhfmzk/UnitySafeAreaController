6d92bbd5bb49152f33b5700c941a78fe875a729d
Sang Hyeon <rlatkdgus500@gmail.com>
2017-12-27 00:37:58 +0900

    Init project - setup scene

---
+++ Assets/SafeAreaController/SafeAreaController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AreaUpdateTiming {
+    onAwake,
+    onStart,
+    onUpdate,
+    onFixedUpdate
+};
+
+public class SafeAreaController : MonoBehaviour {
+
+    public AreaUpdateTiming updateTimming = AreaUpdateTiming.onAwake;
+
+    private Rect safeArea;
+
+    void UpdateSafeArea() {
+        this.safeArea = Screen.safeArea;
+
+
+    } 
+
+    void Awake() {
+        if(updateTimming == AreaUpdateTiming.onAwake) {
+        }
+    }
+
+    void Start () {
+        if(updateTimming == AreaUpdateTiming.onStart) {
+        }
+	}
+	
+	void Update () {
+        if(updateTimming == AreaUpdateTiming.onUpdate) {
+        }
+	}
+
+    void FixedUpdate() {
+        if(updateTimming == AreaUpdateTiming.onFixedUpdate) {
+        }
+    }
+}
