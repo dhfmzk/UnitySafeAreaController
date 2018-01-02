2c9a19b7f93edf7551691d7da966c8c9a378a063
Sang Hyeon <rlatkdgus500@gmail.com>
2018-01-03 06:24:34 +0900

    Make base logic and add 'in editor test' button

---
+++ Assets/SafeAreaController/Editor/SafeAreaControllerEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (SafeAreaController))]
+public class SafeAreaControllerEditor : Editor {
+
+	public eSafeAreaType testType;
+	public bool isSafeAreaOn;
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI ();
+
+		SafeAreaController saController = target as SafeAreaController;
+
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Safe-Area Test in Editor");
+
+		testType = (eSafeAreaType)EditorGUILayout.EnumPopup("Test Device Type:", testType);
+		
+		if(!isSafeAreaOn) {
+			if (GUILayout.Button("Show Safe-Area")) {
+				if (saController) {
+					saController.ShowSafeAreaFrame(testType);
+					isSafeAreaOn = true;
+				}
+			}
+		}
+		else {
+			if (GUILayout.Button("Hide Safe-Area") ) {
+				if (saController) {
+					saController.HideSafeAreaFrame(testType);
+					isSafeAreaOn = false;
+				}
+			}
+		}
+
+		EditorGUILayout.Space();
+	}
+}
--- Assets/SafeAreaController/SafeAreaController.cs
+++ Assets/SafeAreaController/SafeAreaController.cs
@@ -1,43 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public enum AreaUpdateTiming {
+public enum eAreaUpdateTiming {
     onAwake,
     onStart,
     onUpdate,
     onFixedUpdate
 };
 
+public enum eSafeAreaType {
+	IPHONE_X_TALL,
+	IPHONE_X_WIDE
+}
+
 public class SafeAreaController : MonoBehaviour {
 
-    public AreaUpdateTiming updateTimming = AreaUpdateTiming.onAwake;
+	public eAreaUpdateTiming updateTimming = eAreaUpdateTiming.onAwake;
 
-    private Rect safeArea;
+	public List<RectTransform> safeAreaList;
 
-    void UpdateSafeArea() {
-        this.safeArea = Screen.safeArea;
+	private Rect safeArea;
+	private RectTransform rootRectTransform = null;
 
+	private Vector2 saAnchorMin;
+	private Vector2 saAnchorMax;
+
+	private void UpdateSafeArea(Rect _safeArea, Vector2 _screen) {
+
+		if(rootRectTransform == null) {
+			this.rootRectTransform = this.GetComponent<RectTransform>();
+		}
+
+		// Get Safe Area and calc anchor (float)
+		this.saAnchorMin.x = _safeArea.x / _screen.x;
+		this.saAnchorMin.y = _safeArea.y / _screen.y;
+		this.saAnchorMax.x = (_safeArea.x + _safeArea.width) / _screen.x;
+		this.saAnchorMax.y = (_safeArea.y + _safeArea.height) / _screen.y;
+
+
+		// Set anchor
+		this.rootRectTransform.anchorMin = saAnchorMin;
+		this.rootRectTransform.anchorMax = saAnchorMax;
+
+		// Set anchor onther roots
+		foreach(RectTransform rt in safeAreaList) {
+			rt.anchorMin = saAnchorMin;
+			rt.anchorMax = saAnchorMax;
+		}
 
     } 
 
-    void Awake() {
-        if(updateTimming == AreaUpdateTiming.onAwake) {
+	private void Awake() {
+		if(updateTimming == eAreaUpdateTiming.onAwake) {
+			UpdateSafeArea (Screen.safeArea, new Vector2(Screen.width, Screen.height));
         }
     }
 
-    void Start () {
-        if(updateTimming == AreaUpdateTiming.onStart) {
+	private void Start () {
+		if(updateTimming == eAreaUpdateTiming.onStart) {
+			UpdateSafeArea (Screen.safeArea, new Vector2(Screen.width, Screen.height));
         }
 	}
 	
-	void Update () {
-        if(updateTimming == AreaUpdateTiming.onUpdate) {
+	private void Update () {
+		if(updateTimming == eAreaUpdateTiming.onUpdate) {
+			UpdateSafeArea (Screen.safeArea, new Vector2(Screen.width, Screen.height));
         }
 	}
 
-    void FixedUpdate() {
-        if(updateTimming == AreaUpdateTiming.onFixedUpdate) {
+	private void FixedUpdate() {
+		if(updateTimming == eAreaUpdateTiming.onFixedUpdate) {
+			UpdateSafeArea (Screen.safeArea, new Vector2(Screen.width, Screen.height));
         }
     }
+
+// =================================================================
+// 		Functions 4 Editor
+// =================================================================
+	public void ShowSafeAreaFrame(eSafeAreaType _type) {
+		
+		// Listing SafeArea Device
+		if (_type == eSafeAreaType.IPHONE_X_TALL) { UpdateSafeArea(new Rect (0, 102, 1125, 2202), new Vector2(1125.0f, 2436.0f)); }
+		if (_type == eSafeAreaType.IPHONE_X_WIDE) { UpdateSafeArea(new Rect (102, 0, 2202, 1125), new Vector2(2436.0f, 1125.0f)); }
+
+	}
+
+	public void HideSafeAreaFrame(eSafeAreaType _type) {
+
+		UpdateSafeArea (new Rect(0, 0, 1, 1), new Vector2(1, 1));
+
+	}
 }
