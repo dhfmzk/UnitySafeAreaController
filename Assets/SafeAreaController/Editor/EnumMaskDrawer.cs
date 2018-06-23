bcccb420876f18976a72e18a50bd4569fb3abd1a
Sang Hyeon <rlatkdgus500@gmail.com>
2018-06-23 15:17:13 +0900

    #Logic #Add enum mask attribute

---
+++ Assets/SafeAreaController/Editor/EnumMaskDrawer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(EnumMask))]
+public class EnumMaskDrawer : PropertyDrawer {
+    
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumNames);
+    }
+}
